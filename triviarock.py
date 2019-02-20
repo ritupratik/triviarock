6ead5d96ecf3dfa59c85b6a638ded6be7e892ec7
ritupratik <47660530+ritupratik@users.noreply.github.com>
2019-02-20 10:57:37 +0530

    Update triviarock.py

--- triviarock.py
+++ triviarock.py
@@ -21,7 +21,7 @@ async def on_member_join(member):
 
 @client.event    
 async def on_ready():
-    await client.change_presence(game=Game(name="subscription proccess||Help, type=3"))
+    await client.change_presence(game=Game(name="subscription proccess||Help", type = 3))
     print('Ready, Freddy') 
 
 
